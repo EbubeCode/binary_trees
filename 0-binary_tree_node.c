1739901fca14c9859f05195c9a752b461a421d81
EbubeCode <chukwuma258@gmail.com>
2022-08-23 18:14:05 +0100

    task 0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -17,8 +17,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 		return (NULL);
 
 	node->n = value;
-	if (parent)
-		node->parent = parent;
+	node->parent = parent;
 
 	return (node);
 }
