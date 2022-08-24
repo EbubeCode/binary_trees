260072f23960407c8ed34444f67a92f4365ec490
EbubeCode <chukwuma258@gmail.com>
2022-08-24 10:44:20 +0100

    task 8

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_postorder - trasverse a binary tree postorder
+ * @tree: pointer to the root of the tree
+ * @func: pointer to function to call for each node
+ */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+		return;
+
+	if (tree->left)
+		binary_tree_postorder(tree->left, func);
+
+	if (tree->right)
+		binary_tree_postorder(tree->right, func);
+
+	func(tree->n);
+}
