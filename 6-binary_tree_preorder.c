973c37334a3235723d7d066586d0358ca02345d4
EbubeCode <chukwuma258@gmail.com>
2022-08-24 10:32:03 +0100

    task 6

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - trasverse a binary tree in preorder
+ * @tree: pointer to the root of the tree
+ * @func: pointer to function to call for each node
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+		return;
+
+	func(tree->n);
+
+	if (tree->left)
+		binary_tree_preorder(tree->left, func);
+	if (tree->right)
+		binary_tree_preorder(tree->right, func);
+}
