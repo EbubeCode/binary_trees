65af4e780d59910bdf42dbe472cbb4917e4015b0
EbubeCode <chukwuma258@gmail.com>
2022-08-24 15:13:25 +0100

    task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -53,19 +53,21 @@ int is_full(const binary_tree_t *tree)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	size_t r, l;
+	size_t r = 0, l = 0;
 
 	if (!tree)
 		return (0);
 	if (!tree->right && !tree->left)
 		return (1);
 
-	r = height(tree->right);
-	l = height(tree->left);
+	if (tree->right)
+		r = height(tree->right);
+	if (tree->left)
+		l = height(tree->left);
 
 	if (r != l)
 		return (0);
-	if (is_full(tree->left) && is_full(tree->right))
+	if (is_full(tree))
 		return (1);
 	return (0);
 }
